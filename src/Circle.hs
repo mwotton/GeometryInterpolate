3ff63afe65da86ffaa412b8ba6bc1fa2ff20cfe7
Mark Wotton <mwotton@gmail.com>
2018-05-01 21:34:26 -0400

    derive foldable

--- src/Circle.hs
+++ src/Circle.hs
@@ -1,17 +1,19 @@
+{-# LANGUAGE DeriveFoldable             #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
+{-# LANGUAGE InstanceSigs               #-}
 module Circle where
 
 newtype Tolerance a = Tolerance a
 
 newtype X a = X a
+  deriving Foldable
 newtype Y a = Y a
+  deriving Foldable
 newtype Point a = Point (X a, Y a)
+  deriving Foldable
 
 newtype PolyLine a = PolyLine [Point a]
-instance Foldable PolyLine where
-  foldMap :: (Monoid m, Foldable t) => (a -> m) -> t a -> m
-  foldMap f (PolyLine pl) = foldMap f pl
+  deriving Foldable
 
 newtype Radius a = Radius a
 newtype Circle a = Circle (Point a, Radius a)
